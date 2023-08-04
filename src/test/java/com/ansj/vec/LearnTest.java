1aaabddf7a734e5c93b186fd070a994599b0e0da
mikusugar <syfangjie@live.cn>
2023-08-04 11:25:40 +0800

    refactor: 修改资源文件位置

--- src/test/java/com/ansj/vec/LearnTest.java
+++ src/test/java/com/ansj/vec/LearnTest.java
@@ -4,16 +4,19 @@ import java.io.File;
 import java.io.IOException;
 
 import com.ansj.vec.Learn;
+import me.mikusugar.node2vec.HelpTestUtils;
 
 public class LearnTest
 {
 
     public static void main(String[] args) throws IOException
     {
+        final String corpusFilePath = HelpTestUtils.getResourcePath() + "/corpus.txt";
         Learn learn = new Learn();
         learn.setLayerSize(200);
         learn.setMAX_EXP(10);
-        learn.learnFile(new File("corpus.txt"));
+
+        learn.learnFile(new File(corpusFilePath));
         learn.saveModel(new File("model.bin"));
     }
 }
